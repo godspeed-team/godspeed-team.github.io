a033bfe5fb89607411172c6df3d7cbf93af1d16f
Rainer Jung <Rainer.Jung@gmail.com>
2017-07-08 19:03:27 +0200

    rename menu entries (fixes #1)

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ class App extends Component {
         <nav>
           <ul ref={(list) => { this.list = list }}>
             <li><b>Work and travel as Team</b></li>
-            <li><a href="">Build a Team</a></li>
-            <li><a href="">Provide a Team</a></li>
-            <li><a href="">Recruit a Team</a></li>
-            <li className="burger"><button onClick={this.toggleMenu}>&#9776;</button></li>
+            <li><a href="">Build</a></li>
+            <li><a href="">Provide</a></li>
+            <li><a href="">Recruit</a></li>
+            <li className="burger"><a href="" onClick={this.toggleMenu}>&#9776;</a></li>
           </ul>
         </nav>
       </div>
