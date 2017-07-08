492b54bf2569e67ee50e72e722aa0561ff1b0505
Rainer Jung <Rainer.Jung@gmail.com>
2017-07-08 18:46:18 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(e) {
+    e.preventDefault();
+    if(this.list.className === "responsive") {
+      this.list.className = "";
+    } else {
+      this.list.className = "responsive";
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <div className="Earth" />
-          <div className="App-intro">godspeed team</div>
-        </div>
+        <nav>
+          <ul ref={(list) => { this.list = list }}>
+            <li><b>Work and travel as Team</b></li>
+            <li><a href="">Build a Team</a></li>
+            <li><a href="">Provide a Team</a></li>
+            <li><a href="">Recruit a Team</a></li>
+            <li className="burger"><a href="javascript:void(0);" onClick={this.toggleMenu}>&#9776;</a></li>
+          </ul>
+        </nav>
       </div>
     );
   }
